1c8e6cf382477ebd00386d3d7d64b4af1ec01fd0
Kazuto <Kazuto@EQ002.mshome.net>
2018-11-29 09:16:59 +0100

    Ejercicio 3 -> Comentado

--- Exercicio3/src/main/Hilo.java
+++ Exercicio3/src/main/Hilo.java
@@ -2,22 +2,31 @@ package main;
 
 public class Hilo extends Thread {
     
-    private static int numero = 0;
+    private static int numero = 0; // Cuenta el número de hilos que se generara en el programa
     private Hilo hilo;
     
     public Hilo(){
-            System.out.println("Creando el (" + this.getName() + ")");
-            numero++;
-            this.start();
+        // Muestro que el hilo se esta creando
+        
+        System.out.println("Creando el (" + this.getName() + ")");
+        
+        // Aumento el contador de hilos generados
+        // Inicio en hilo actual
+        
+        numero++;
+        this.start();
             
-            
-            if (numero < 5){
+        // Compruebo si se han generado menos de 5 hilos
+        // Si son menos genera hilo otro nuevo
+        if (numero < 5){
                     
-                hilo = new Hilo();
-            }
+            hilo = new Hilo();
+        }
     }
     
     @Override public void run() {
+        
+        // Se muestra 10 veces por pantalla con un delay de 0,6s
         
         for(int i = 0; i < 10; i++){
             
@@ -28,6 +37,9 @@ public class Hilo extends Thread {
             } catch (InterruptedException ex) {}
         }
         
+        // Comprueba si el hilo actual es el Thread 4 (Hilo nº5)
+        // Ya que si es llega 4 deja de generar nuevos hilos
+        
         try {
             if(this.getName().equalsIgnoreCase("Thread-4")){
                 
@@ -35,6 +47,8 @@ public class Hilo extends Thread {
                 hilo.join();
             }
         } catch (InterruptedException ex) {}
+        
+        // Muestra por pantalla cuendo se finaliza el hilo
         
         System.out.println("Acabando el (" + this.getName() + ")");
     }
