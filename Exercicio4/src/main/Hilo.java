c46d0978983cc65e648c2afb34af794b4e342dc5
Kazuto <Kazuto@EQ002.mshome.net>
2018-11-29 09:24:41 +0100

    Ejercicio 4 -> Comentado

--- Exercicio4/src/main/Hilo.java
+++ Exercicio4/src/main/Hilo.java
@@ -4,22 +4,30 @@ public class Hilo extends Thread {
 
     private Hilo hilo;
     private int numero = 0;
-    private int modo;
+    private int modo; // Define que operacion se le asignara al hilo
     
     public Hilo(int modo){
+            // Muestro por pantalla que el hilo se creo
+        
             System.out.println("Creando el (" + this.getName() + ")");
-
             this.modo = modo;
+            
+            // Inicio el hilo
+            
             this.start();
     }
     
     @Override public void run() {
+        
+        // Segun el nº pasado al constructor el hilo hara X operación
         
         switch(modo){
             case 1: sumPares(); break;
             case 2: sumImpares(); break;
             case 3: sumOtros(); break;
         }
+        
+        // Muestro por pantalla que acaba el hilo
         
         System.out.println("Acabando el (" + this.getName() + ")");
     }
@@ -28,7 +36,7 @@ public class Hilo extends Thread {
     
     public void sumPares(){
         
-        for(int i = 1; i <= 1000; i++){
+        for(int i = 1; numero <= 1000; i++){
             
             if (i % 2 == 0) {
                 
@@ -47,7 +55,7 @@ public class Hilo extends Thread {
     
     public void sumImpares(){
         
-        for(int i = 1; i <= 1000; i++){
+        for(int i = 1; numero <= 1000; i++){
             
             if (i % 2 != 0) {
                 
@@ -66,7 +74,7 @@ public class Hilo extends Thread {
     
     public void sumOtros(){
         
-        for(int i = 1; i <= 1000; i++){
+        for(int i = 1; numero <= 1000; i++){
             String[] array = String.valueOf(i).split("");
             
             if (Integer.parseInt(array[array.length -1]) == 2 || Integer.parseInt(array[array.length -1]) == 3) {
--- Exercicio4/src/main/Main.java
+++ Exercicio4/src/main/Main.java
@@ -4,6 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
         
+        // Genero 3 hilos
+        
         new Hilo(1);
         new Hilo(2);
         new Hilo(3);
