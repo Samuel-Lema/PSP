f7368b17a3967615e288f96217e16787357869b1
Kazuto <Kazuto@EQ002.mshome.net>
2018-12-03 10:56:52 +0100

    Ejercicio 8 -> Comentado

---
+++ Exercicio8/src/main/Animal.java
@@ -0,0 +1,93 @@
+package main;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Animal extends Thread {
+
+    public final String nombre;
+    public final int tipoAnimal; // 0 -> Tortuga, 1 -> Liebre
+    public int casilla = 1;
+
+    // Constructor
+     public  Animal(String nombre, int tipoAnimal) {
+
+        this.nombre = nombre;
+        this.tipoAnimal = tipoAnimal;
+        this.start();
+    }
+
+    public synchronized void turno() {
+
+        int mov = (int) (Math.random() * 100);
+
+        if (tipoAnimal == 0) { // Tortuga
+            
+            if (mov >= 0 && mov < 50) { // +3 casilla (50%) -> Avance rápido
+
+                casilla += 3;
+
+            } else if (mov >= 50 && mov < 70) { // -6 casilla (20%) -> Esvarou
+
+                casilla -= 6;
+
+            } else if (mov >= 70 && mov < 100) { // +1 casilla (30%) -> Avance lento
+
+                casilla += 1;
+            }
+
+        } else if (tipoAnimal == 1) { // Liebre
+            
+            if (mov >= 0 && mov < 20) { // 0 casilla (20%) -> Dorme
+
+            } else if (mov >= 20 && mov < 40) { // +9 casilla (20%) -> Gran Salto
+
+                casilla += 9;
+
+            } else if (mov >= 40 && mov < 50) { // -12 casilla (10%) -> Esvarón grande
+
+                casilla -= 12;
+
+            } else if (mov >= 50 && mov < 80) { // +1 casilla (30%) -> Pequeno Salto
+
+                casilla += 1;
+
+            } else if (mov >= 80 && mov < 100) { // -2 casilla (20%) -> Esvarón pequeno
+
+                casilla -= 2;
+            }
+        }
+
+        if (casilla < 1) {
+
+            casilla = 1;
+        }
+        
+        System.out.println(this.nombre + ": " + casilla);
+        
+        notify();
+        try {
+            wait();
+        } catch (InterruptedException ex) {
+            Logger.getLogger(Animal.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    @Override synchronized public void run() {
+        
+        try {
+            while (casilla < 70) {
+                this.turno();
+                Animal.sleep(600);
+            }
+
+            switch (tipoAnimal) {
+                case 0: Main.liebre.stop(); break;
+                case 1: Main.tortuga.stop(); break;
+                default: System.out.println("WTF"); break;
+            }
+            
+            System.out.println(" -------------- La " + this.nombre + " ha ganado. --------------");
+        } catch (InterruptedException ex) {}
+    }
+}
---
+++ Exercicio8/src/main/Main.java
@@ -0,0 +1,14 @@
+package main;
+
+public class Main {
+
+    public static Animal tortuga;
+    public static Animal liebre;
+    
+    public static void main(String[] args) {
+        
+        tortuga = new Animal("-> Tortuga", 0);
+        liebre = new Animal("Liebre", 1);
+    }
+    
+}
