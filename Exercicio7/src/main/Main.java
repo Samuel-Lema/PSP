e8791b1c018aa7ffe0a81a1d841c77c0cbf5aa44
Kazuto <Kazuto@EQ002.mshome.net>
2018-12-03 13:49:56 +0100

    Ejercicio 7 -> Comentado

--- Exercicio7/src/main/Main.java
+++ Exercicio7/src/main/Main.java
@@ -27,6 +27,7 @@ public class Main {
         Usuario usuario1 = new Usuario("Samuel", buzon1, buzon2, "Hola buenas, soy Samuel.");
         Usuario usuario2 = new Usuario("Alejandro", buzon2, buzon1, "Hola buenas, soy Alejandro");
         Usuario usuario3 = new Usuario("Alberto", buzon1, buzon2, "Hola buenas, soy Alberto"); */
+        
     }   
     
 }
